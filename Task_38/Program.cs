7fa3920c7f2cb93236c7b19be8868aec334d581a
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-24 19:00:31 +0300

    modified:   README.md new file:   Task_38/Program.cs

---
+++ Task_38/Program.cs
@@ -0,0 +1,43 @@
+﻿Random rnd = new Random();
+double[] nums = new double [10];
+int n = nums.Length;
+double min;
+double max;
+double diff;
+
+for (int i = 0; i < n; i++)
+{
+    nums[i] = rnd.Next(0, 100);
+}
+
+min = nums[0];
+max = nums[0];
+for (int i = 0; i < n; i++)
+{
+    if (nums[i] > max)
+    {
+        max = nums[i];
+    }
+    if (nums[i] < min)
+    {
+        min = nums[i];
+    }
+}
+
+diff = max - min;
+PrintArray(nums);
+Console.WriteLine($" -> разница между максимальным элементом {max} и минимальным элементом {min} массива равна: {diff}");
+
+void PrintArray(double[] arr)
+{
+    string str = "[";
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        str += $", {arr[i]}";
+    }
+
+    str = str.Remove(1, 2);
+    str += "]";
+    Console.Write($"Массив: {str}");
+}
